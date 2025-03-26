bd088aca58fdb09d3aedb50e464c6b4031699ee5
pagrey <pagrey@gmail.com>
2025-03-26 14:06:23 -0700

    simplify music

--- rofi-music.sh
+++ rofi-music.sh
@@ -5,20 +5,39 @@ if ! command -v ffplay 2>&1 >/dev/null; then
     exit 0
 fi
 
-DISCONNECT="Disconnect"
-PAD=" "
-FAVORITE="Cinemix"
-
 declare -rA url=(["Radio Dismuke"]="http://stream1.early1900s.org:8080"
 ["1920s Radio Network"]="http://208.85.242.72:8398"
-["1940s Radio"]="http://199.189.111.28:8012"
 ["1940s UK Radio"]="http://91.121.134.23:8100"
 ["Big Blue Swing"]="http://209.236.126.18:8002"
-["Electro Lounge"]="https://electrolounge.stream.laut.fm/electrolounge?pl=pls&t302=2024-08-26_23-57-55&uuid=ba9e3f02-429f-4659-9ae5-acf6715fb367"
+["Electro Lounge"]="https://electrolounge.stream.laut.fm/electrolounge?pl=pls"
 ["Lounge Radio"]="http://nl1.streamhosting.ch:80"
 ["Venice Classical"]="http://116.202.241.212:8010"
 ["Instrumental Hits Radio"]="https://panel.retrolandigital.com:8130/listen"
 ["Cinemix"]="http://51.81.46.118:1190")
+
+FAVORITE="Cinemix"
+
+DISCONNECT="Disconnect"
+PAD=" "
+HTTP="//"
+SHORTLENGTH=25
+
+print_row(){
+    address=${url["$entry"]}
+    i=$(expr index "$address" "$HTTP")
+    if [[ ${#address} -gt $SHORTLENGTH ]]; then
+	shorturl="${address:$i+1:$SHORTLENGTH}..."
+    else
+	shorturl="${address:$i+1}"
+    fi
+    if [[ $nowplaying = $address ]]; then
+	stationplaying="$entry"
+	echo -e "\0active\x1f$COUNTER"
+	echo -e "$entry\0display\x1f$PAD$entry <span color='#555'> $shorturl</span>\x1fnonselectable\x1ftrue"
+    else
+	echo -e "$entry\0display\x1f$PAD$entry <span color='#555'> $shorturl</span>"
+    fi
+}
 
 nowplaying=$(ps hw -C ffplay -o args= | sed -e 's/^.*nodisp //')
 
@@ -44,27 +63,16 @@ if [[ -n $nowplaying ]]; then
     	echo -e "$DISCONNECT\0permanent\x1ftrue"
 fi
 
-if [[ $nowplaying = ${url["$FAVORITE"]} ]]; then
-	stationplaying="$FAVORITE"
-	echo -e "\0active\x1f1"
-    echo -e "$FAVORITE\0display\x1f$PAD$FAVORITE\x1fnonselectable\x1ftrue"
-else
-	echo -e "$FAVORITE\0display\x1f$PAD$FAVORITE"
-fi
+entry=$FAVORITE
+COUNTER=1
+print_row
+let COUNTER++
 
-COUNTER=2
-	
 for entry in "${!url[@]}"
 do
     if [[ "$entry" != $FAVORITE ]]; then
-		if [[ $nowplaying = ${url["$entry"]} ]]; then
-			stationplaying="$entry"
-			echo -e "\0active\x1f$COUNTER"
-			echo -e "$entry\0display\x1f$PAD$entry\x1fnonselectable\x1ftrue"
-		else
-			echo -e "$entry\0display\x1f$PAD$entry"
-		fi
-		let COUNTER++
+	print_row
+	let COUNTER++
     fi
 done
 
