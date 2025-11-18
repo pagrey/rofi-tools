48dea4a1c716877b71360d550657cd9cfaf1fbc4
pagrey <pagrey@gmail.com>
2025-11-18 09:31:56 -0800

    add screensaver tool

---
+++ rofi-xset.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -eu
+
+DEFAULT="600"
+RESET="10 minutes ($DEFAULT)"
+DISABLE="Disable"
+ACTIVATE="Activate"
+OFF="0"
+
+
+if ! command -v xset 2>&1 >/dev/null; then
+    echo "xset not found!"
+    exit 0
+fi
+
+if [[ $# -gt 0 ]]; then
+	if [[ $1 =~ ^[0-9]+$ ]]; then
+	    xset dpms $1 $1 $1
+	    xset s $1 $1
+	    exit 0
+	else
+		case "$1" in
+			"$ACTIVATE")
+				sleep 0.25 && xset s activate
+				exit 0
+				;;
+			"$RESET")
+				xset dpms $DEFAULT $DEFAULT $DEFAULT 
+				xset s $DEFAULT $DEFAULT 
+				exit 0
+				;;
+			"$DISABLE")
+				xset dpms $OFF $OFF $OFF
+				xset s $OFF $OFF
+				exit 0
+				;;
+		esac
+	fi
+fi
+
+standby=`xset q | sed -n -e '/Standby/p' | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/'`
+timeout=`xset q | sed -n -e '/timeout/p' | sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/'`
+
+echo -e "\0prompt\x1fEnter screensaver timeout (seconds)"
+echo -e "\0markup-rows\x1ftrue"
+echo -e "\0keep-selection\x1ftrue"
+if [[ $standby != $OFF ]]; then
+	echo -e "\0message\x1f<b>Current timeout:</b> $standby"
+else
+	echo -e "\0message\x1f<b>Current timeout:</b> disabled"
+fi
+
+echo -e "$ACTIVATE"
+echo -e "$RESET"
+echo -e "$DISABLE"
